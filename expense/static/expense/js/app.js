f5d1449254f83198e652a2a2bfc329146b360980
Sumit Jamgade <sumitj@invicto.in>
2015-09-12 05:26:54 +0530

    collapsing panels. new icon, delete confirmation, can edit

--- expense/static/expense/js/app.js
+++ expense/static/expense/js/app.js
@@ -1,6 +1,4 @@
-angular.module('managerapp', [
-        "xeditable"
-        ])
+angular.module('managerapp', [ ])
 .config(function($interpolateProvider){
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
@@ -32,6 +30,27 @@ angular.module('managerapp', [
             console.log('Fuck!!');
             console.log(what);
         });
+    $scope.delExpense = function(data, cb){
+        if(data.hasOwnProperty('id')){
+            url = '/expense/'+data.id;
+            $http.delete(url)
+                .success(function(response){
+                    console.log("okay");
+                    delete($scope.explist[data.id]);
+                    if (!!cb)
+                        cb(true);
+                })
+                .error(function(error){
+                    console.log("data");
+                    console.log(data);
+                    console.log("Error");
+                    console.log(error);
+                    if (!!cb)
+                        cb(false);
+                })
+            }
+
+    }
     $scope.saveExpense = function(data, cb){
         url = "/expense";
         if (data.hasOwnProperty('id')){
@@ -65,36 +84,40 @@ angular.module('managerapp', [
 })
 .directive("expense",function(){
     return{
-        replace:true,
         retrict:'A',
         scope : {
             ob:"=",
-            updateParent:"&"
+            updateParent:"&",
+            deleteParent:"&"
         },
         controller:function($scope){
-                $scope.toggleEdit = function(){
-                    $scope.editing = !$scope.editing;
-                };
-                $scope.repost = function(edit){
-                    var dat = angular.copy($scope.ob);
-                    var cb = undefined;
-                    if(!!!edit){
-                        delete(dat.id);
-                    }
-                    else{
-                        cb = function(done){
-                            if(done){
-                                $scope.toggleEdit()
-                            }
+            $scope.deleting = false;
+            $scope.toggleEdit = function(){
+                $('#ob'+$scope.ob.id ).collapse('hide');
+            };
+            $scope.repost = function(edit){
+                var dat = angular.copy($scope.ob);
+                var cb = undefined;
+                if(!!!edit){
+                    delete(dat.id);
+                }
+                else{
+                    cb = function(done){
+                        if(done){
+                            $scope.toggleEdit()
                         }
                     }
-                    $scope.updateParent({d:dat, cb:cb});
-                };
-                $scope.pinIt = function(){
-                    dat = {'id':$scope.ob.id, 'pinned':! $scope.ob.pinned};
-                    $scope.updateParent({d:dat});
-                };
-            },
+                }
+                $scope.updateParent({d:dat, cb:cb});
+            };
+            $scope.pinIt = function(){
+                dat = {'id':$scope.ob.id, 'pinned':! $scope.ob.pinned};
+                $scope.updateParent({d:dat});
+            };
+            $scope.delete = function(){
+                $scope.deleteParent({d:{id:$scope.ob.id}}) 
+            }
+        },
         templateUrl: ANGULAR_TEMPLATE_PATH +"expense/tmpl/exp_tmpl.html",
     };
 });
