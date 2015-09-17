d1c2fc8f8e46208f9cae51802da615ea6b2ceea1
Sumit Jamgade <sumitj@invicto.in>
2015-09-18 00:11:01 +0530

    export data for google spreadsheet

--- manager/urls.py
+++ manager/urls.py
@@ -8,4 +8,4 @@ from expense import urls as expense_urls
 urlpatterns = patterns('',
         url(r'^$',TemplateView.as_view(template_name="index.html")))
 
-urlpatterns += user_urls.router.urls + expense_urls.router.urls
+urlpatterns += user_urls.router.urls + expense_urls.urlpatterns
