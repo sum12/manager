f8197df5180b90f07d204d19f67fa135b28cbafc
Sumit Jamgade <sumitj@invicto.in>
2015-10-01 19:44:32 +0530

    mend

--- local_settings.py
+++ local_settings.py
@@ -1,5 +1,7 @@
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-ALLOWED_HOSTS = ["intense-falls-3111.herokuapp.com"]
+#ALLOWED_HOSTS = ["www.intense-falls-3111.herokuapp.com"]
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
 DEBUG = False
 TEMPLATE_DEBUG = False
 
