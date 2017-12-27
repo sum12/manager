21e51a7c5664086691a0d1302a791a2d7e16597d
Sumit Jamgade <sjamgade@suse.com>
2017-12-27 17:36:56 +0100

    removed graph feature for now

--- daily/urls.py
+++ daily/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import SimpleRouter
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
-from .views import DailyActivityViewSet, getGraph
+from .views import DailyActivityViewSet
 
 
 router = SimpleRouter(trailing_slash=False)
--- daily/views.py
+++ daily/views.py
@@ -1,16 +1,10 @@
-from django.shortcuts import render_to_response
-from django.http import HttpResponse
-
 from rest_framework import viewsets
 from .models import activity
 from .serializers import ActivitySerializer
 from rest_framework import viewsets, authentication, permissions, filters
 
 import cStringIO
-import matplotlib
 import datetime as dt
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
 
 import logging
 logger = logging.getLogger(__name__)
@@ -40,27 +34,4 @@ class DailyActivityViewSet(DefaultsMixin, viewsets.ModelViewSet):
         qry = activity.objects
         qry = qry.filter(on__gte=until)
         return qry.all()
-
-
-
-
-def getGraph(request, *args, **kwargs):
-    plt.close()
-    fig, ax= plt.subplots(1)
-    activity_type = kwargs['activity_type']
-    obs = activity.objects.filter(type = activity_type).order_by('on')
-    dates = []
-    if activity_type == 'exercise':
-        totals = []
-        counts = []
-        for ob in obs:
-            dates.append(ob.on)
-            splitted = ob.data.split(',')
-            counts.append(len(splitted))
-            totals.append(sum(int(i or 0) for i in splitted))
-        ax.plot(dates, totals,'r-' , dates, counts, 'bs')
-    fig.autofmt_xdate()
-    graph_file = cStringIO.StringIO()
-    fig.savefig(graph_file, format='png')
-    return HttpResponse(graph_file.getvalue(), content_type="image/png")
 
