2b511e75dbe716d175957b6903fb88c9d75296fb
sum12 <sumitjami@gmail.com>
2018-01-06 20:35:07 +0100

    gitignore local_setting.py
    removed local_settings.py

--- local_settings.py
+++
@@ -1,7 +0,0 @@
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#ALLOWED_HOSTS = ["www.intense-falls-3111.herokuapp.com"]
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-ALLOWED_HOSTS = ['*']
-DEBUG = False
-TEMPLATE_DEBUG = False
-
