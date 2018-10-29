b8e1d273ed3885f51f906819f2ead3571136beac
sum12 <sumitjami@gmail.com>
2018-10-29 11:22:15 +0100

    daily: query optimizations
    remove the typeorder object from activity.
    - the object is not really needed and
    - every "activity" obj requires a requery for its type_order object,
    this does not make sense for object when the search itself was performed
    based on type_order_id

--- daily/serializers.py
+++ daily/serializers.py
@@ -2,15 +2,19 @@
 from rest_framework import serializers
 
 import datetime as dt
-from .models import activity,typeorder
+from .models import activity, typeorder
+
+
+class TypeOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = typeorder
+        fields = ('id', 'type', 'order')
 
 
 class ActivitySerializer(serializers.ModelSerializer):
 
     daysbefore = serializers.SerializerMethodField()
     donetoday = serializers.SerializerMethodField()
-    type = serializers.SerializerMethodField()
-    order = serializers.SerializerMethodField()
 
     def get_daysbefore(self, obj):
         today = dt.datetime.today().date()
@@ -20,19 +24,6 @@ class ActivitySerializer(serializers.ModelSerializer):
         today = dt.datetime.today().date()
         return (today - obj.on.date()).days == 0
 
-    def get_type(self, obj):
-        return obj.type_order.type
-
-    def get_order(self, obj):
-        return obj.type_order.order
-
     class Meta:
         model = activity
-        fields = ('id', 'type_order','type', 'order', 'on', 'data', 'daysbefore', 'donetoday')
-
-
-class TypeOrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = typeorder
-        fields = ('id', 'type','order')
-
+        fields = ('id', 'type_order_id', 'on', 'data', 'daysbefore', 'donetoday')
